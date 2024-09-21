e23fdf4dd24bc408255f2d16147709b7f7be0c69
Lara Fernanda <larafvitoriano@gmail.com>
2024-09-20 23:32:45 -0300

    fix: routes, lazy loading, ajustando title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+    path: "", loadComponent: () => import('./componentes/primeiro-componente/primeiro-componente.component')
+    .then(m => m.PrimeiroComponenteComponent)
+},
+{
+    path: "experimento", loadComponent: () => import('./componentes/testando-route/testando-route.component')
+    .then(m => m.TestandoRouteComponent)
+},];
